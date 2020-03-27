8d0410187edd2cc5656cc0bb01f8991a27c8cbbe
Marcel <marcel.dfga@gmail.com>
2020-03-27 14:37:35 +0100

    TInc el collectionCell i customCell

---
+++ LaboratorisFib/Views/CustomCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCollectionViewCell.swift
+//  LaboratorisFib
+//
+//  Created by Marcel Costa i Amezquita on 27/03/2020.
+//  Copyright © 2020 Marcel Costa i Amezquita. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var nomAulari: UILabel!
+    
+}
